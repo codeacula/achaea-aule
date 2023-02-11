abeec6f5770a7f1d63da76a49081d024512d7e4a
Codeacula <codeacula@codeacula.com>
2023-02-11 07:10:47 -0600

    WIP

--- Sailing/Sailing Trading Script.lua
+++ Sailing/Sailing Trading Script.lua
@@ -1,15 +1,29 @@
 aule.sailing.trading = aule.sailing.trading or {}
 aule.sailing.trading.circuitBreakerCount = 0
+aule.sailing.trading.circuitBreakerLimit = 100000
 
-function aule.sailing.trading.createDestination(name, buyWhat, buyCount, sellWhat, sellCount, fees)
+function aule.sailing.trading.circuitBreaker()
+  aule.sailing.trading.circuitBreakerCount = aule.sailing.trading.circuitBreakerCount + 1
+
+  if aule.sailing.trading.circuitBreakerCount >= aule.sailing.trading.circuitBreakerLimit then
+    aule.warn("Hit the circuit breaker.")
+    print(debug.traceback())
+    return false
+  end
+
+  return true
+end
+
+function aule.sailing.trading.createDestination(name, buyWhat, buyCount, sellWhat, sellCount, fees, parent)
   return {
-    name = name,
-    dockingFee = fees,
-    buyWhat = buyWhat,
-    buyCount = buyCount,
-    sellWhat = sellWhat,
-    sellCount = sellCount,
-    paths = {}
+      name = name,
+      dockingFee = fees,
+      buyWhat = buyWhat,
+      buyCount = buyCount,
+      sellWhat = sellWhat,
+      sellCount = sellCount,
+      parent = parent,
+      paths = {}
   }
 end
 
@@ -33,14 +47,38 @@ function aule.sailing.trading.findRoutes(where, what, count)
 
   local dest = aule.sailing.trading.createDestination(where, nil, nil, what, count, finalDestination.fee)
   local finalDest = aule.sailing.trading.processDestination(dest)
+
   display(finalDest)
+  echo("Done!")
   aule.sailing.trading.circuitBreakerCount = 0
+end
+
+function aule.sailing.trading.getListOfCurrentTrades(dest)
+  local currentTrades = {}
+
+  local tempDest = dest
+
+  while tempDest do
+    if not aule.sailing.trading.circuitBreaker() then return end
+
+    currentTrades[tempDest.sellWhat] = true
+    tempDest = tempDest.parent
+  end
+
+  return currentTrades
 end
 
 function aule.sailing.trading.getPortTrades(port, what)
   local trades = {}
+  local alreadyTrading = aule.sailing.trading.getListOfCurrentTrades(dest)
+
+  if not alreadyTrading then
+    aule.warn("Couldn't get a list of already traded items")
+    return {}
+  end
+
   for _, trade in ipairs(port.trades) do
-    if trade.get.what == what then
+    if not alreadyTrading[trade.get.what] and trade.get.what == what then
       trades[#trades + 1] = trade
     end
   end
@@ -48,35 +86,24 @@ function aule.sailing.trading.getPortTrades(port, what)
   return trades
 end
 
-function aule.sailing.trading.incrementAndCheckBreaker()
-  aule.sailing.trading.circuitBreakerCount = aule.sailing.trading.circuitBreakerCount + 1
-
-  -- 100k should be enough depth, right?
-  if aule.sailing.trading.circuitBreakerCount > 100000 then
-    aule.warn("Too many hits")
-    return true
-  end
-
-  return false
-end
-
 function aule.sailing.trading.processDestination(dest)
   -- Go through every port that sells what the harbour
   for name, port in pairs(aule.sailing.ports) do
-    local foundTrades = aule.sailing.trading.getPortTrades(port, dest.sellWhat)
-    aule.sailing.trading.circuitBreakerCount = aule.sailing.trading.circuitBreakerCount + 1
+    if not aule.sailing.trading.circuitBreaker() then return end
+    -- We don't need to revisit ports - this also is an easier way to prevent infinite recursion
+    if not aule.sailing.trading.checkIfWeAlreadyVisit(name, dest) then
+      local foundTrades = aule.sailing.trading.getPortTrades(port, dest.sellWhat)
 
-    if aule.sailing.trading.incrementAndCheckBreaker() then return end
-
-    for _, trade in ipairs(foundTrades) do
-      if aule.sailing.trading.incrementAndCheckBreaker() then return end
-      dest.paths[#dest.paths + 1] = aule.sailing.trading.createDestination(name, trade.get.what, trade.get.amount,
-        trade.pay.what, trade.pay.amount, port.fee)
+      for _, trade in ipairs(foundTrades) do
+        if not aule.sailing.trading.circuitBreaker() then return end
+        dest.paths[#dest.paths + 1] = aule.sailing.trading.createDestination(name, trade.get.what, trade.get.amount,
+                trade.pay.what, trade.pay.amount, port.fee, dest)
+      end
     end
   end
 
   for _, nextDest in ipairs(dest.paths) do
-    if aule.sailing.trading.incrementAndCheckBreaker() then return end
+    if not aule.sailing.trading.circuitBreaker() then return end
     aule.sailing.trading.processDestination(nextDest)
   end
 
